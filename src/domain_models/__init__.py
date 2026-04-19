fcd9f3a0f741c4e7a6b5aeb99c7d6f81ebaaefbd
Felippe Menezes <unchain0@proton.me>
2026-04-19 08:05:36 -0300

    refactor(domain): remove non-domain exports

--- src/domain_models/__init__.py
+++ src/domain_models/__init__.py
@@ -1,16 +1,11 @@
 from src.domain_models.farm_task import FARM_TASK_DESCRIPTION, FARM_TASK_TITLE, FarmTaskDefinition
 from src.domain_models.party_quest_status import PartyQuestStatus
-from src.domain_models.resilience import CircuitBreaker, RetryConfig
-from src.domain_models.settings import Settings
 from src.domain_models.user_status import UserStatus
 
 __all__ = [
-    "CircuitBreaker",
     "FARM_TASK_DESCRIPTION",
     "FARM_TASK_TITLE",
     "FarmTaskDefinition",
     "PartyQuestStatus",
-    "RetryConfig",
-    "Settings",
     "UserStatus",
 ]
